f60eccd30603c7973560bb22e1bb4cc827e0d822
Simone Marcantel☆ <simonemarcantel04@gmail.com>
2022-10-07 10:13:50 -0500

    math functions

---
+++ main.js
@@ -0,0 +1,13 @@
+const fruitsAddition = (apples, oranges) => {
+    const sum = apples + oranges
+
+    return sum
+}
+console.log(fruitsAddition(4, 7))
+
+const fruitsMultiplication = (apple) => {
+    const product = apple * 2
+
+    return product
+}
+console.log(fruitsMultiplication(24))
